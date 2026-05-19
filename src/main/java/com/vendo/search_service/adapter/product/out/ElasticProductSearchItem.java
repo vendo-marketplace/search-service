c261c3b0bdcb3e7ee1f3c226652daccf75db4fd1
HaaaamBurger <gemasclashes@gmail.com>
2026-05-19 22:29:06 +0300

    using elastic annotations for better mapping

--- src/main/java/com/vendo/search_service/adapter/product/out/ElasticProductSearchItem.java
+++ src/main/java/com/vendo/search_service/adapter/product/out/ElasticProductSearchItem.java
@@ -1,5 +1,6 @@
 package com.vendo.search_service.adapter.product.out;
 
+import org.springframework.data.annotation.Id;
 import org.springframework.data.elasticsearch.annotations.Document;
 import org.springframework.data.elasticsearch.annotations.Field;
 import org.springframework.data.elasticsearch.annotations.FieldType;
@@ -10,21 +11,45 @@ import java.util.List;
 
 @Document(indexName = "products")
 public record ElasticProductSearchItem(
+
+        @Id
         String id,
+
+        @Field(type = FieldType.Text)
         String title,
+
+        @Field(type = FieldType.Text)
         String description,
+
+        @Field(type = FieldType.Integer)
         Integer quantity,
+
+        @Field(type = FieldType.Double)
         BigDecimal price,
+
+        @Field(type = FieldType.Keyword)
         String ownerId,
+
+        @Field(type = FieldType.Keyword)
         String categoryId,
+
+        @Field(type = FieldType.Nested)
         List<ElasticSearchAttribute> attributes,
+
+        @Field(type = FieldType.Boolean)
         Boolean active,
+
         @Field(type = FieldType.Date)
         Instant createdAt
 ) {
 
     public record ElasticSearchAttribute(
+
+            @Field(type = FieldType.Keyword)
             String id,
-            List<String> values) {
+
+            @Field(type = FieldType.Keyword)
+            List<String> values
+    ) {
     }
 }
