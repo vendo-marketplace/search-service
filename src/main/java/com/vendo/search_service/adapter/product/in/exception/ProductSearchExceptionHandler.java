dbce9da84000fb5e8537bb500693f661d6d91368
HaaaamBurger <gemasclashes@gmail.com>
2026-07-01 10:45:45 +0300

    kan-66: refactoring

--- src/main/java/com/vendo/search_service/adapter/product/in/exception/ProductSearchExceptionHandler.java
+++ src/main/java/com/vendo/search_service/adapter/product/in/exception/ProductSearchExceptionHandler.java
@@ -3,13 +3,11 @@ package com.vendo.search_service.adapter.product.in.exception;
 import com.vendo.search_service.domain.product.exception.InternalSearchException;
 import com.vendo.security_lib.exception.response.ExceptionResponse;
 import jakarta.servlet.http.HttpServletRequest;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@Slf4j
 @RestControllerAdvice
 public class ProductSearchExceptionHandler {
 
