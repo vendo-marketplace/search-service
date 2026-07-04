5b9fa5e7a0785a8f185f6ed427853b340a6952da
HaaaamBurger <gemasclashes@gmail.com>
2026-07-04 09:24:48 +0300

    kan-66: fixing tests

--- src/main/java/com/vendo/search_service/adapter/product/out/ElasticSearchAttribute.java
+++ src/main/java/com/vendo/search_service/adapter/product/out/ElasticSearchAttribute.java
@@ -1,5 +1,6 @@
 package com.vendo.search_service.adapter.product.out;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import org.springframework.data.elasticsearch.annotations.Field;
 import org.springframework.data.elasticsearch.annotations.FieldType;
@@ -7,6 +8,7 @@ import org.springframework.data.elasticsearch.annotations.FieldType;
 import java.util.List;
 
 @Data
+@AllArgsConstructor
 public  final class ElasticSearchAttribute {
 
     @Field(type = FieldType.Keyword)
