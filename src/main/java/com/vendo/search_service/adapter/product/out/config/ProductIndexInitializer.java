f223cde89f90135e1ba2b3e34abf99a6871014e6
HaaaamBurger <gemasclashes@gmail.com>
2026-07-04 09:58:12 +0300

    kan-66: logging

--- src/main/java/com/vendo/search_service/adapter/product/out/config/ProductIndexInitializer.java
+++ src/main/java/com/vendo/search_service/adapter/product/out/config/ProductIndexInitializer.java
@@ -2,12 +2,14 @@ package com.vendo.search_service.adapter.product.out.config;
 
 import com.vendo.search_service.adapter.product.out.ElasticProductSearchItem;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.event.EventListener;
 import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
 import org.springframework.data.elasticsearch.core.IndexOperations;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class ProductIndexInitializer {
@@ -19,6 +21,7 @@ public class ProductIndexInitializer {
         IndexOperations indexOps = operations.indexOps(ElasticProductSearchItem.class);
 
         if (!indexOps.exists()) {
+            log.info("Initializing index for product.");
             indexOps.create();
             indexOps.putMapping();
         }
