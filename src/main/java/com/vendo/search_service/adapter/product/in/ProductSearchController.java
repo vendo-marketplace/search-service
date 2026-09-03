363450661961b39ee6f4d6bad21943f6c44f1a18
HaaaamBurger <gemasclashes@gmail.com>
2026-09-03 18:27:43 +0300

    refactoring

--- src/main/java/com/vendo/search_service/adapter/product/in/ProductSearchController.java
+++ src/main/java/com/vendo/search_service/adapter/product/in/ProductSearchController.java
@@ -24,7 +24,6 @@ class ProductSearchController {
             @Valid @RequestBody(required = false) ProductSearchRequest request
     ) {
         ProductSearchData data = productSearchUseCase.search(q, mapper.toSearchItem(request));
-        System.out.println(data);
         return ResponseEntity.ok(data);
     }
 
